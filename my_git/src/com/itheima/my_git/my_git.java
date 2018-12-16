7c1705b847444573985510c3df39d57c8c9eeb6c
李金朋 <389776289@qq.com>
2018-12-16 18:15:42 +0800

    我上传了!!!!

---
+++ my_git/src/com/itheima/my_git/my_git.java
@@ -0,0 +1,8 @@
+package com.itheima.my_git;
+
+public class my_git {
+    public static void main(String[] args) {
+        System.out.println("丢雷老母");
+        System.out.println("丢雷老母");
+    }
+}
